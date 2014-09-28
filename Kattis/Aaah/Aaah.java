ad75494de03a393082dea3d7177c4500916faf07
Thomas Breier <breierthomas@gmail.com>
2014-09-27 22:45:23 -0700

    cleanup Aaah

--- Kattis/Aaah/Aaah.java
+++ Kattis/Aaah/Aaah.java
@@ -1,3 +1,13 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.OutputStream;
+
+
 public class Aaah {
 
 	public static void main(String[] args) {	
@@ -9,6 +19,7 @@ public class Aaah {
 			String doctor = io.getWord();
 
 			System.out.println(jon.length() >= doctor.length() ? "go" : "no");
+			break;
 
 		}
 		
@@ -18,4 +29,90 @@ public class Aaah {
 
 }
 
+
+/** Simple yet moderately fast I/O routines.
+ *
+ * Example usage:
+ *
+ * Kattio io = new Kattio(System.in, System.out);
+ *
+ * while (io.hasMoreTokens()) {
+ *    int n = io.getInt();
+ *    double d = io.getDouble();
+ *    double ans = d*n;
+ *
+ *    io.println("Answer: " + ans);
+ * }
+ *
+ * io.close();
+ *
+ *
+ * Some notes:
+ *
+ * - When done, you should always do io.close() or io.flush() on the
+ *   Kattio-instance, otherwise, you may lose output.
+ *
+ * - The getInt(), getDouble(), and getLong() methods will throw an
+ *   exception if there is no more data in the input, so it is generally
+ *   a good idea to use hasMoreTokens() to check for end-of-file.
+ *
+ * @author: Kattis
+ */
+
+class Kattio extends PrintWriter {
+    public Kattio(InputStream i) {
+  super(new BufferedOutputStream(System.out));
+  r = new BufferedReader(new InputStreamReader(i));
+    }
+    public Kattio(InputStream i, OutputStream o) {
+  super(new BufferedOutputStream(o));
+  r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public boolean hasMoreTokens() {
+  return peekToken() != null;
+    }
+
+    public int getInt() {
+  return Integer.parseInt(nextToken());
+    }
+
+    public double getDouble() { 
+  return Double.parseDouble(nextToken());
+    }
+
+    public long getLong() {
+  return Long.parseLong(nextToken());
+    }
+
+    public String getWord() {
+  return nextToken();
+    }
+
+
+
+    private BufferedReader r;
+    private String line;
+    private StringTokenizer st;
+    private String token;
+
+    private String peekToken() {
+  if (token == null) 
+      try {
+    while (st == null || !st.hasMoreTokens()) {
+        line = r.readLine();
+        if (line == null) return null;
+        st = new StringTokenizer(line);
+    }
+    token = st.nextToken();
+      } catch (IOException e) { }
+  return token;
+    }
+
+    private String nextToken() {
+  String ans = peekToken();
+  token = null;
+  return ans;
+    }
+}
 
--- Kattis/Aaah/readme.md
+++ Kattis/Aaah/readme.md
@@ -1,1 +1,9 @@
 https://open.kattis.com/problems/aaah
+
+```bash
+$ javac Aaah.java 
+$ cmp <(for f in tests/*.in; do java Aaah < $f; done) <(for f in tests/*.ans; do cat $f; done)
+$ # (no output)
+$ java Aaah < tests/aaah.2.in
+go
+```
